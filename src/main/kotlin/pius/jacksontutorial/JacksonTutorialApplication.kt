e28ac2fabd97f4a49d70b36a6a4a8052e7d1afb0
pius712 <aksfbsgnlfls@gmail.com>
2023-10-02 23:01:09 +0900

    first commit

---
+++ src/main/kotlin/pius/jacksontutorial/JacksonTutorialApplication.kt
@@ -0,0 +1,11 @@
+package pius.jacksontutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JacksonTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<JacksonTutorialApplication>(*args)
+}
